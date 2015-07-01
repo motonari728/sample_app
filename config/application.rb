7338533bb4449438fc57054640ec434d7d33dfb4
Motonari Tsuzuki <your.email@example.com>
2015-07-01 15:35:32 +0900

    cross site

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,11 @@ module SampleApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.action_dispatch.default_headers = {
+    #'Access-Control-Allow-Credentials' => 'true',
+    'Access-Control-Allow-Origin' => '*',
+    'Access-Control-Request-Method' => '*'
+    }
   end
 end
 
